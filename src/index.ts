14dd3b818484c0d7dadb0e9a985f9c9a505cbb5a
Ruslan Dzhafarov <ruslan_dzhafarov@epam.com>
2023-01-15 21:22:30 +0300

    add validation

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,41 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import { User } from "./user";
+import { validateBody } from "./validation";
 
 const PORT = 3000;
-const app = express();
-app.use(express.json());
-class User {
-  id: string;
-  login: string;
-  password: string;
-  age: number;
-  isDeleted: boolean;
 
-  constructor(login: string, password: string, age: number) {
-    this.id = uuidv4();
-    this.login = login;
-    this.password = password;
-    this.age = age;
-    this.isDeleted = false;
-  }
-}
+const app = express();
+
+app.use(express.json());
 
 const users: User[] = [];
 
 app.get("/users/:id", (req, res) => {
   const user = users.find((user) => user.id === req.params.id);
+
   if (!user) {
+    console.log(`User ${req.params.id} not found`);
     return res.status(404).send({ message: "User not found" });
   }
+
+  console.log(`User ${user.id} has been found`);
+
   res.send(user);
 });
 
-app.post("/users", (req, res) => {
+app.post("/users", validateBody, (req, res) => {
   const { login, password, age } = req.body;
+
   const user = new User(login, password, age);
+
   users.push(user);
+
+  console.log(`User ${user.id} has been added`);
+
   res.send(user);
 });
 
-app.put("/users/:id", (req, res) => {
+app.put("/users/search/:id", validateBody, (req, res) => {
   const user = users.find((user) => user.id === req.params.id);
   if (!user) {
     return res.status(404).send({ message: "User not found" });
@@ -56,13 +54,16 @@ app.put("/users/:id", (req, res) => {
 
 app.get("/users", (req, res) => {
   const { loginSubstring, limit = 20 } = req.query;
+
   console.log(
-    `Request performed with queries loginSubstring: ${loginSubstring}; limit: ${limit}`
+    `GET /users request performed with queries loginSubstring: ${loginSubstring}; limit: ${limit}`
   );
+
   if (!loginSubstring) {
     const limitedUsers = users.slice(0, limit as number);
     return res.send(limitedUsers);
   }
+
   const filteredUsers = users.filter(
     (user) => user.login.includes(loginSubstring as string) && !user.isDeleted
   );
@@ -70,20 +71,27 @@ app.get("/users", (req, res) => {
     a.login.localeCompare(b.login)
   );
   const limitedUsers = sortedUsers.slice(0, limit as number);
+
   res.send(limitedUsers);
 });
 
 app.delete("/users", (req, res) => {
   const { id } = req.body;
   const user = users.find((user) => user.id === id);
+
   if (!user) {
     return res.status(404).send({ message: "User not found" });
   }
+
   user.isDeleted = true;
-  console.log(`User ${user.id} has been deleted`);
+
+  console.log(
+    `DELETE /users request performed. User ${user.id} has been deleted`
+  );
+
   res.send(user);
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on 3000 port");
+  console.log(`Server is running on ${PORT} port`);
 });
